b4e43ceaf92c9404a46f2388c68415d130c89ddf
Ramzan Chasygov <theliketurbo@gmail.com>
2019-03-31 21:25:51 +0300

    translation, rotation, scale

--- src/chapter03/index.ts
+++ src/chapter03/index.ts
@@ -23,21 +23,113 @@ const fragmentShader = require('./shaders/fragment.glsl');
 
   const aPosition = gl.getAttribLocation(gl.program, 'a_Position');
   const uFragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-  const uTranslation = gl.getUniformLocation(gl.program, 'u_Translation');
-  const uCosA = gl.getUniformLocation(gl.program, 'u_CosA');
-  const uSinA = gl.getUniformLocation(gl.program, 'u_SinA');
+  const uRotationMatrix = gl.getUniformLocation(gl.program, 'u_Rotation');
+  const uTranslationMatrix = gl.getUniformLocation(gl.program, 'u_Translation');
+  const uScalingMatrix = gl.getUniformLocation(gl.program, 'u_Scaling');
 
-  if (aPosition < 0 || !uFragColor || !uTranslation || !uCosA || !uSinA)
+  if (
+    aPosition < 0 ||
+    !uFragColor ||
+    !uRotationMatrix ||
+    !uTranslationMatrix ||
+    !uScalingMatrix
+  )
     return err('fgst');
 
   gl.vertexAttrib3f(aPosition, 0, 0, 0);
 
-  const pos = [0.0, 0.0, 0.0];
-  gl.uniform4f(uTranslation, pos[0], pos[1], pos[2], 0.0);
-
   const angle = Math.PI / 2;
-  gl.uniform1f(uCosA, Math.cos(angle));
-  gl.uniform1f(uSinA, Math.sin(angle));
+  const tX = 0.25,
+    tY = 0.0,
+    tZ = 0.0;
+  const sX = 1.25,
+    sY = 1.25,
+    sZ = 1.25;
+
+  /**
+   * [
+   *  [cos, sin, 0, 0]
+   *  [-sin, cos, 0, 0]
+   *  [0, 0, 1, 0]
+   *  [0, 0, 0, 1]
+   * ]
+   */
+  const rotationMatrix = new Float32Array([
+    Math.cos(angle),
+    Math.sin(angle),
+    0.0,
+    0.0,
+    -Math.sin(angle),
+    Math.cos(angle),
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    1.0
+  ]);
+
+  /**
+   * [
+   *  [1, 0, 0, x]
+   *  [0, 1, 0, y]
+   *  [0, 0, 1, z]
+   *  [0, 0, 0, 1]
+   * ]
+   */
+  const translationMatrix = new Float32Array([
+    1,
+    0,
+    0,
+    tX,
+    0,
+    1,
+    0,
+    tY,
+    0,
+    0,
+    1,
+    tZ,
+    0,
+    0,
+    0,
+    1
+  ]);
+
+  /**
+   * [
+   *  [x, 0, 0, 0]
+   *  [0, y, 0, 0]
+   *  [0, 0, z, 0]
+   *  [0, 0, 0, 1]
+   * ]
+   */
+  const scalingMatrix = new Float32Array([
+    sX,
+    0,
+    0,
+    0,
+    0,
+    sY,
+    0,
+    0,
+    0,
+    0,
+    sZ,
+    0,
+    0,
+    0,
+    0,
+    1
+  ]);
+
+  gl.uniformMatrix4fv(uTranslationMatrix, false, translationMatrix);
+  gl.uniformMatrix4fv(uRotationMatrix, false, rotationMatrix);
+  gl.uniformMatrix4fv(uScalingMatrix, false, scalingMatrix);
 
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
--- src/chapter03/shaders/vertex.glsl
+++ src/chapter03/shaders/vertex.glsl
@@ -1,9 +1,8 @@
 attribute vec4 a_Position;
-uniform vec4 u_Translation;
-uniform float u_CosA, u_SinA;
+uniform mat4 u_Rotation;
+uniform mat4 u_Translation;
+uniform mat4 u_Scaling;
 
 void main() {
-  gl_Position = a_Position + u_Translation;
-  gl_Position.x = a_Position.x * u_CosA - a_Position.y * u_SinA;
-  gl_Position.y = a_Position.x * u_SinA + a_Position.y * u_CosA;
+  gl_Position = ((u_Rotation * a_Position) * u_Translation) * u_Scaling;
 }
