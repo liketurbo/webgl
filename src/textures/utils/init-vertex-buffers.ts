bcf41fe357d0377665bc95477620f38df12f0b20
Ramzan Chasygov <theliketurbo@gmail.com>
2019-04-04 23:24:04 +0300

    added multi size feature

--- src/textures/utils/init-vertex-buffers.ts
+++ src/textures/utils/init-vertex-buffers.ts
@@ -1,26 +1,40 @@
+import { WebGLContext } from '../../common/types/webgl';
 import err from '../../common/utils/error';
 
-const initVertexBuffers = (
-  gl: WebGLRenderingContext & { program: WebGLProgram }
-) => {
-  var vertices = new Float32Array([0.0, 0.3, -0.3, -0.3, 0.3, -0.3]);
-  var n = 3; // The number of vertices
+const initVertexBuffers = (gl: WebGLContext) => {
+  const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  const n = 3;
+
+  const sizes = new Float32Array([
+    10.0,
+    20.0,
+    30.0 // Point sizes
+  ]);
 
   // Create a buffer object
-  var vertexBuffer = gl.createBuffer();
-  if (!vertexBuffer) return err('fcbo');
+  const vertexBuffer = gl.createBuffer();
+  const sizeBuffer = gl.createBuffer();
+  if (!vertexBuffer || !sizeBuffer) return err('fcbo');
 
-  // Bind the buffer object to target
+  // Write vertex coordinates to the buffer object and enable it
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  // Write date into the buffer object
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
   const aPosition = gl.getAttribLocation(gl.program, 'a_Position');
+  const aPointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  if (aPointSize < 0 || aPosition < 0) return err('fgsl');
 
-  // Assign the buffer object to a_Position variable
   gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-  // Enable the assignment to a_Position variable
   gl.enableVertexAttribArray(aPosition);
+
+  // Bind the point size buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
+  gl.vertexAttribPointer(aPointSize, 1, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPointSize);
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   return n;
 };
