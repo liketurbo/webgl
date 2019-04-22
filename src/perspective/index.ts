5ef00b966a9979b8441e6a3738b3968eba4ef913
Ramzan Chasygov <theliketurbo@gmail.com>
2019-04-22 18:11:35 +0300

    moved matrix calculations to js (ts)

--- src/perspective/index.ts
+++ src/perspective/index.ts
@@ -21,40 +21,44 @@ const fragmentShader = require('./shaders/fragment.glsl');
   const n = initVertexBuffers(gl);
   if (n < 0) return err('fspv');
 
-  // Get the storage location of u_ViewMatrix variable
-  const uViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
-  if (!uViewMatrix) return err('fgsl', 'u_ViewMatrix');
-
-  const uProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
-  if (!uProjMatrix) return err('fgsl', 'u_ProjMatrix');
-
   const uModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   if (!uModelMatrix) return err('fgsl', 'u_ModelMatrix');
 
-  const modelMatrix = new Matrix4();
+  let modelMatrix = new Matrix4();
   modelMatrix.setTranslate(0.75, 0, 0);
-  gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix.elements);
 
   const viewMatrix = new Matrix4();
   viewMatrix.setLookAt(0, 0, 5, 0, 0, -100, 0, 1, 0);
-  // Set the eye point, look-at point, and up direction
-
-  // Pass the view matrix to u_ViewMatrix variable
-  gl.uniformMatrix4fv(uViewMatrix, false, viewMatrix.elements);
 
   const projMatrix = new Matrix4();
   projMatrix.setPerspective(30, canvas.width / canvas.height, 1, 100);
-  gl.uniformMatrix4fv(uProjMatrix, false, projMatrix.elements);
 
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw a triangle
+  gl.uniformMatrix4fv(
+    uModelMatrix,
+    false,
+    new Matrix4()
+      .set(projMatrix)!
+      .multiply(viewMatrix)
+      .multiply(modelMatrix).elements
+  );
+
+  // Draw a triangles
   gl.drawArrays(gl.TRIANGLES, 0, n);
 
   modelMatrix.setTranslate(-0.75, 0, 0);
-  gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix.elements);
-  // Draw a triangle
+  gl.uniformMatrix4fv(
+    uModelMatrix,
+    false,
+    new Matrix4()
+      .set(projMatrix)!
+      .multiply(viewMatrix)
+      .multiply(modelMatrix).elements
+  );
+
+  // Draw a triangles
   gl.drawArrays(gl.TRIANGLES, 0, n);
 
   return 0;
